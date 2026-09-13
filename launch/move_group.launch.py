8537c94d88cdbeb8c3c72f455909fe06232d6bfd
Ryan McKee <ryanmckee47@icloud.com>
2026-09-13 18:28:15 +0100

    fine tuning params

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -1,7 +1,25 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
 
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("abb_irb140", package_name="abb_irb140_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+    
+    # Move Group Node
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
+            {"publish_robot_description_semantic": True},
+            {"use_sim_time": True},
+        ],
+    )
+
+    return LaunchDescription(
+        [move_group_node]
+    )
